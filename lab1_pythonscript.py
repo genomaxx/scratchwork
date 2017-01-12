fc2cc865ba32ef48a33d9d2487fd30d2925e987f
md6113 <jmdougla@ualberta.ca>
2017-01-11 23:35:35 -0700

    question 8 modifications added

--- lab1_pythonscript.py
+++ lab1_pythonscript.py
@@ -1,5 +1,9 @@
 import requests
 s = requests.__version__
 r = requests.get('https://google.com')
+q = requests.get('https://github.com/md6113/scratchwork/blob/master/lab1_pythonscript.py')
+p = requests.get('https://raw.githubusercontent.com/md6113/scratchwork/master/lab1_pythonscript.py')
 print(s)
 print(r)
+print(q)
+print(p)
